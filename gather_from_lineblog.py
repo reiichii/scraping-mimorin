747af2972d830bed8afc8d3dddbcb58994d3fb1d
reiichii <passion.engk@gmail.com>
2019-05-05 23:14:14 +0900

    create scraping a page

--- gather_from_lineblog.py
+++ gather_from_lineblog.py
@@ -1,2 +1,9 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
+
+r = requests.get('https://lineblog.me/mimori_suzuko/')
+soup = BeautifulSoup(r.content, 'html.parser')
+divs = soup.find_all('div', class_='article-body')
+for div in divs:
+    img = div.find_all('img', width='600')
+    print(img)
