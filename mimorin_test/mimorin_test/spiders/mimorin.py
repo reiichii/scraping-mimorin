1c4c7db04227ce3f66b200c3e2f8adc5f44b2b81
reiichii <passion.engk@gmail.com>
2019-05-07 21:59:28 +0900

    get next page url

--- mimorin_test/mimorin_test/spiders/mimorin.py
+++ mimorin_test/mimorin_test/spiders/mimorin.py
@@ -7,8 +7,12 @@ class MimorinSpider(scrapy.Spider):
     allowed_domains = ['lineblog.me/mimori_suzuko']
     start_urls = ['http://lineblog.me/mimori_suzuko/']
 
+
     def parse(self, response):
         # title出力
         h1_titles = response.xpath('//h1/a/text()').extract()
         for title in h1_titles:
             print(title+'\n')
+        next_page = response.xpath('//*[@id="main-inner"]/div[3]/div/div/ul/li[1]/ol/li[2]/a/@href').extract()
+        print(next_page[0])
+        scrapy.Request(next_page[0], callback=self.parse)
