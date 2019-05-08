89d03416bb6897f1b674102dd3f3c2851561aeac
reiichii <passion.engk@gmail.com>
2019-05-08 20:36:48 +0900

    add cache setting

--- mimorin_test/mimorin_test/settings.py
+++ mimorin_test/mimorin_test/settings.py
@@ -83,7 +83,7 @@ DOWNLOAD_DELAY = 3
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
+HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
