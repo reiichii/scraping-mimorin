292def69ce53ecbbbf42b759ca01407bcb58654c
reiichii <passion.engk@gmail.com>
2019-05-12 00:55:14 +0900

    create next page render

--- mimorin_test/mimorin_test/spiders/mimorin.py
+++ mimorin_test/mimorin_test/spiders/mimorin.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import sys
 from mimorin_test.items import MimorinItem
 
 class MimorinSpider(scrapy.Spider):
     name = 'mimorin'
     allowed_domains = ['lineblog.me']
     start_urls = ['https://lineblog.me/mimori_suzuko/']
+    current_page = 1
 
 
     def parse(self, response):
@@ -14,8 +16,15 @@ class MimorinSpider(scrapy.Spider):
         item['image_urls'] = []
         for image_url in response.css('img.pict::attr(src)').extract():
             item['image_urls'].append(image_url)
+        print(item)
 
-        return item
+        #総ページ数を取得
+        total_page = response.css('li.paging-last a::text').extract_first()
+        if total_page is None:
+            sys.exit()
 
-        # next_url = 'https://lineblog.me/mimori_suzuko/?p=2'
-        # yield scrapy.Request(next_url, callback=self.parse)
+        # 総ページ分回してurlを生成
+        if self.current_page <= 5:
+            self.current_page += 1
+            next_url = 'https://lineblog.me/mimori_suzuko/?p=' + str(self.current_page)
+            yield scrapy.Request(next_url, callback=self.parse)
