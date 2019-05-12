5016524a40d5ce6ccd9a6667f72c46e10477f67b
reiichii <passion.engk@gmail.com>
2019-05-12 23:56:42 +0900

    create scrapy mimorin in blog

--- mimorin_test/mimorin_test/spiders/mimorin.py
+++ mimorin_test/mimorin_test/spiders/mimorin.py
@@ -9,22 +9,26 @@ class MimorinSpider(scrapy.Spider):
     start_urls = ['https://lineblog.me/mimori_suzuko/']
     current_page = 1
 
-
     def parse(self, response):
         # そのページの画像を取得
-        item = MimorinItem()
-        item['image_urls'] = []
-        for image_url in response.css('img.pict::attr(src)').extract():
-            item['image_urls'].append(image_url)
-        print(item)
-
-        #総ページ数を取得
+        yield self.parse_items(response)
+        
+        # 総ページ数を取得
         total_page = response.css('li.paging-last a::text').extract_first()
         if total_page is None:
             sys.exit()
-
         # 総ページ分回してurlを生成
-        if self.current_page <= 5:
+        if self.current_page <= int(total_page):
             self.current_page += 1
             next_url = 'https://lineblog.me/mimori_suzuko/?p=' + str(self.current_page)
             yield scrapy.Request(next_url, callback=self.parse)
+            
+    
+    def parse_items(self, response):
+        item = MimorinItem()
+        item['image_urls'] = []
+            
+        for image in response.css('img.pict::attr(src)').extract():
+            item['image_urls'].append(image)         
+
+        return item
